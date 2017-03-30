cf14d0a3b421df73e17fa51563936f68e68658c3
Saidur Rahman <saidur2k@hotmail.com>
2017-03-30 22:01:08 +1100

    add sorting to getPodcasts route

--- api/podcasts/getPodcasts.js
+++ api/podcasts/getPodcasts.js
@@ -1,22 +1,37 @@
 'use strict'
 
 const podcastsData = require('../../data/podcasts')
+const sortBy = require('lodash').sortBy
 
 module.exports = {
   method: 'GET',
   path: '/api/podcasts',
   config: {
     handler: (request, reply) => {
-    const trimmedPodcastsData = podcastsData.map(
-      (podcast) => {
-        return {
-          id: podcast.id,
-          name: podcast.name,
-          slug: podcast.slug
+      const trimmedPodcastsData = podcastsData.map(
+        (podcast) => {
+          return {
+            id: podcast.id,
+            name: podcast.name,
+            slug: podcast.slug
+          }
+        }
+      )
+
+      const sortDirection = request.query.sortDirection
+      const sortKey = request.query.sortKey
+
+      const sortData = (data, direction, key) => {
+        if (direction === 'asc') {
+          return sortBy(data, key)
+        } else if (direction === 'desc') {
+          return sortBy(data, key).reverse()
+        } else {
+          return data
         }
       }
-    )
-      reply(trimmedPodcastsData)
+
+      reply(sortData(trimmedPodcastsData, sortDirection, sortKey))
     }
   }
 }
