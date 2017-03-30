2185c67fbc55860b9eb2e5930a7b1b19b47f69bd
Saidur Rahman <saidur2k@hotmail.com>
2017-03-30 21:06:26 +1100

    add sample data

---
+++ data/podcasts.js
@@ -0,0 +1,35 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Podcast A',
+    website: 'www.podcast-a.com',
+    twitter: '@pdocastA',
+    slug: 'podcast-a',
+    episodes: [
+      'episode 1',
+      'episode 2'
+    ]
+  },
+  {
+    id: 2,
+    name: 'Podcast B',
+    website: 'www.podcast-b.com',
+    twitter: '@pdocastB',
+    slug: 'podcast-b',
+    episodes: [
+      'episode 3',
+      'episode 4'
+    ]
+  },
+  {
+    id: 3,
+    name: 'Podcast C',
+    website: 'www.podcast-c.com',
+    twitter: '@pdocastC',
+    slug: 'podcast-c',
+    episodes: [
+      'episode 5',
+      'episode 6'
+    ]
+  }
+]
